61d86df6ba0538e0b8e79c405f6dafd902036e6b
Pranav K <prkrishn@hotmail.com>
2018-06-25 11:00:56 -0700

    WIP

--- src/Microsoft.AspNetCore.Mvc.Analyzers/ApiControllerSymbolCache.cs
+++ src/Microsoft.AspNetCore.Mvc.Analyzers/ApiControllerSymbolCache.cs
@@ -1,6 +1,10 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+<<<<<<< HEAD
+=======
+using System;
+>>>>>>> WIP
 using Microsoft.CodeAnalysis;
 
 namespace Microsoft.AspNetCore.Mvc.Analyzers
@@ -9,6 +13,7 @@ namespace Microsoft.AspNetCore.Mvc.Analyzers
     {
         public ApiControllerSymbolCache(Compilation compilation)
         {
+<<<<<<< HEAD
             ApiConventionTypeAttribute = compilation.GetTypeByMetadataName(SymbolNames.ApiConventionTypeAttribute);
             ApiConventionNameMatchAttribute = compilation.GetTypeByMetadataName(SymbolNames.ApiConventionNameMatchAttribute);
             ApiConventionTypeMatchAttribute = compilation.GetTypeByMetadataName(SymbolNames.ApiConventionTypeMatchAttribute);
@@ -20,6 +25,31 @@ namespace Microsoft.AspNetCore.Mvc.Analyzers
         public INamedTypeSymbol ApiConventionTypeMatchAttribute { get; }
 
         public INamedTypeSymbol ApiConventionTypeAttribute { get; }
+=======
+            ApiConventionAttribute = compilation.GetTypeByMetadataName(SymbolNames.ApiConventionAttribute);
+            ControllerAttribute = compilation.GetTypeByMetadataName(SymbolNames.ControllerAttribute);
+            IApiBehaviorMetadata = compilation.GetTypeByMetadataName(SymbolNames.IApiBehaviorMetadata);
+            NonActionAttribute = compilation.GetTypeByMetadataName(SymbolNames.NonActionAttribute);
+            NonControllerAttribute = compilation.GetTypeByMetadataName(SymbolNames.NonControllerAttribute);
+            ProducesResponseTypeAttribute = compilation.GetTypeByMetadataName(SymbolNames.ProducesResponseTypeAttribute);
+
+            var disposable = compilation.GetSpecialType(SpecialType.System_IDisposable);
+            var members = disposable.GetMembers(nameof(IDisposable.Dispose));
+            IDisposableDispose = members.Length == 1 ? (IMethodSymbol)members[0] : null;
+        }
+
+        public INamedTypeSymbol ApiConventionAttribute { get; }
+
+        public INamedTypeSymbol ControllerAttribute { get; }
+
+        public INamedTypeSymbol IApiBehaviorMetadata { get; }
+
+        public IMethodSymbol IDisposableDispose { get; }
+
+        public INamedTypeSymbol NonActionAttribute { get; }
+
+        public INamedTypeSymbol NonControllerAttribute { get; }
+>>>>>>> WIP
 
         public INamedTypeSymbol ProducesResponseTypeAttribute { get; }
     }
