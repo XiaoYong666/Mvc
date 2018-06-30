2eff7fae7cffb28c0120f34ac57162fafe724220
Pranav K <prkrishn@hotmail.com>
2018-06-30 08:10:59 -0700

    WIP

---
+++ test/Mvc.Analyzers.Test/TestFiles/ApiConventionAnalyzerIntegrationTest/DiagnosticsAreReturned_ForActionResultOfTReturningMethodWithoutSomeAttributes.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[assembly: ApiConventionType(typeof(DefaultApiConventions))]
+
+namespace Microsoft.AspNetCore.Mvc.Analyzers
+{
+    [ApiController]
+    public class DiagnosticsAreReturned_ForActionResultOfTReturningMethodWithoutSomeAttributes : ControllerBase
+    {
+        [ProducesResponseType(typeof(string), 200)]
+        [ProducesResponseType(typeof(string), 404)]
+        public IActionResult Put(int id, object model)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                /*MM*/return UnprocessableEntity();
+            }
+
+            return Ok();
+        }
+    }
+}
